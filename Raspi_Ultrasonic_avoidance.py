4b9c8bac4fb155ba4d417d7d1669314ff4b6102b
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2024-01-14 20:25:11 +0100

    Update with beeping

--- Raspi_Ultrasonic_avoidance.py
+++ Raspi_Ultrasonic_avoidance.py
@@ -1,17 +1,18 @@
-# connection of 3 sensors to RasPi https://i.imgur.com/GcZRI8S.jpg
+from statistics import mean
 import RPi.GPIO as GPIO
 import time
-#import gpiozero
+from pydub import AudioSegment
+from pydub.generators import Sine
+import simpleaudio as sa
+
 # Set GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-# Set pins for the left sensor
-TRIG_LEFT = 24  # Update with your GPIO pin number
-ECHO_LEFT = 23  # Update with your GPIO pin number
-
-# Set pins for the right sensor
-TRIG_RIGHT = 27  # Update with your GPIO pin number
-ECHO_RIGHT = 22  # Update with your GPIO pin number
+# Set pins for the sensors
+TRIG_LEFT = 24
+ECHO_LEFT = 23
+TRIG_RIGHT = 27
+ECHO_RIGHT = 22
 
 # Set up GPIO direction (IN / OUT)
 GPIO.setup(TRIG_LEFT, GPIO.OUT)
@@ -46,17 +47,56 @@ def get_distance(trig, echo):
 
     return distance
 
+
+
+def play_beep(frequency, balance):
+    # Generate a sine wave tone
+    tone = Sine(frequency).to_audio_segment(duration=100)
+
+    # Adjust balance
+    # Balance -1.0 is full left, 1.0 is full right
+    pan = balance
+    tone = tone.pan(pan)
+
+    # Play the tone
+    play_obj = sa.play_buffer(tone.raw_data, num_channels=2, bytes_per_sample=tone.sample_width, sample_rate=tone.frame_rate)
+
+    return play_obj
+
+# start of Main
+# initialise variables
+c_l = (TRIG_LEFT, ECHO_LEFT)
+b_l = c_l
+a_l =c_l
+
+c_r = (TRIG_RIGHT, ECHO_RIGHT)
+b_r = c_r
+a_r =c_r
+
 try:
     while True:
         distance_left = get_distance(TRIG_LEFT, ECHO_LEFT)
-        print("Distance left:"+str(distance_left))
         distance_right = get_distance(TRIG_RIGHT, ECHO_RIGHT)
-        print("Distance right:"+str(distance_right))
 
-        if distance_left < 50.0:
-            print("Go right")
-        elif distance_right < 50.0:
-            print("Go left")
+        # average distance
+        c_l = b_l
+        b_l = a_l
+        a_l = distance_left
+
+        averaged_distance_left = mean(a_l, b_l, c_l)
+
+         # average distance
+        c_r = b_r
+        b_r = a_r
+        a_r = distance_right
+
+        averaged_distance_right = mean(a_r, b_r, c_r)       
+
+        # Determine which side to beep and set balance
+        if averaged_distance_left < 50.0:
+            play_obj = play_beep(1000, -1.0)  # Beep on the left
+        elif averaged_distance_right < 50.0:
+            play_obj = play_beep(1000, 1.0)   # Beep on the right
 
         # Delay for a bit before the next check
         time.sleep(1)
@@ -64,10 +104,3 @@ try:
 except KeyboardInterrupt:
     print("Measurement stopped by user")
     GPIO.cleanup()
-
-## other method
-
-# from gpiozero import DistanceSensor
-# ultrasonic = DistanceSensor(echo=17, trigger=4)
-# while True:
-#     print(ultrasonic.distance)
